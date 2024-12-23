fc96e830e75f4c70c4dd4d937ac9c83c500e70de
Possanii <gpossanii@outlook.com>
2024-12-23 13:01:17 -0300

    feat: Add dotenv for environment variable management - Update start:dev script to load environment variables - Use environment variable for API port in index.ts

---
+++ src/config/env.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+
+const envSchema = z.object({
+  DATABASE_URL: z.string().url(),
+  API_BASE_PORT: z.coerce.number().positive().default(3000),
+});
+
+export const env = envSchema.parse(process.env);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import { env } from './config/env';
 import app from './server/lib/fastify';
 import './server/routes';
 
-app.listen({ port: 3000 }, (err, address) => {
+app.listen({ port: env.API_BASE_PORT }, (err, address) => {
   if (err) {
     app.log.error(err);
     process.exit(1);
